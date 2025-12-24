3a18bffe5d55fb96a188939d11c2a5b2f1cdcee5
shanomm <lolformon@gmail.com>
2025-12-24 05:23:43 +0000

    new

---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from streamlit_tags import st_tags, st_tags_sidebar
+
+st.write("# Code for streamlit tags")
+
+st.code(body='''keywords = st_tags(
+    label='# Enter Keywords:',
+    text='Press enter to add more',
+    value=['Zero', 'One', 'Two'],
+    suggestions=['five', 'six', 'seven', 'eight', 'nine', 'three', 'eleven', 'ten', 'four'],
+    maxtags = 4,
+    key='1')''',
+        language="python")
+
+maxtags = st.slider('Number of tags allowed?', 1, 10, 3, key='jfnkerrnfvikwqejn')
+
+keywords = st_tags(
+    label='Enter Keywords:',
+    text='Press enter to add more',
+    value=['Zero', 'One', 'Two'],
+    suggestions=['five', 'six', 'seven', 'eight', 'nine', 'three', 'eleven', 'ten', 'four'],
+    maxtags=maxtags,
+    key="aljnf")
+
+st.write("### Results:")
+st.write(type(keywords))
+
+st.sidebar.write("# Code for streamlit tags sidebar")
+
+st.sidebar.code(body='''keyword = st_tags_sidebar(
+label='# Enter Keywords:',
+text='Press enter to add more',
+value=['Zero', 'One', 'Two'],
+suggestions=['five', 'six', 'seven', 
+'eight', 'nine', 'three', 
+'eleven', 'ten', 'four'],
+maxtags = 4)''',
+                language="python")
+
+maxtags_sidebar = st.sidebar.slider('Number of tags allowed?', 1, 10, 3, key='ehikwegrjifbwreuk')
+
+
+keyword = st_tags_sidebar(label='# Enter Keywords:',
+                          text='Press enter to add more',
+                          value=['Zero', 'One', 'Two'],
+                          suggestions=['five', 'six', 'seven', 'eight', 'nine', 'three', 'eleven', 'ten', 'four'],
+                          maxtags=maxtags_sidebar,
+                          key="afrfae")
+
+st.sidebar.write("### Results:")
+st.sidebar.write((keyword))
